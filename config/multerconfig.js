3fff7cc3dfc1e009e3f2f39347ddf97ca91178bf
Vighnesh dattatray kachare <129893676+viggghnesh1711@users.noreply.github.com>
2024-09-14 03:34:18 +0530

    Update multerconfig.js

--- config/multerconfig.js
+++ config/multerconfig.js
@@ -1,7 +1,7 @@
 const multer = require('multer')
 const path = require('path')
 const crypto = require('crypto')
-const { storage } = require('./cloudinaryConfig');
+// const { storage } = require('./cloudinaryConfig');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
